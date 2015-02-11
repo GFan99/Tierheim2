a8aad1d6fc377c0f31c4c2e99a09f9b9c3d4fc16
Müller Christiane Schule <schueler@Mueller-Christiane.local>
2015-02-11 10:27:07 +0100

    Halter
    Neue Klasse und Text

---
+++ src/Halter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author schueler
+ */
+import java.util.*;
+        
+public class Halter {
+    String haVorname;
+    String haNachname;
+    int haPLZ;
+    String haStraße;
+    String haOrt;
+    int haTel;
+    String haMail;
+    Date haGebDat;
+    boolean haAufDauer;
+    ArrayList <String> haTiere;
+    int haNr;
+    
+}
