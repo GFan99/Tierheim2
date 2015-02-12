470f926b84f5ca60a64fdf2b26251df578cb607e
GFan99 <becksusanna@Susannas-MacBook-Pro.local>
2015-02-12 15:24:00 +0100

    Personal ist fertig...

--- src/Personal.java
+++ src/Personal.java
@@ -22,5 +22,65 @@ public class Personal {
     public String pMail;
     public float gehalt;
     
-    public void setAdresse
+    public void setAdresse(String strNr, int plz, String ort) {
+        pStrNr = strNr;
+        pPLZ = plz;
+        pOrt = ort;
+    }
+    public void setNName(String name) {
+        pName = name;
+    }
+    public void setTel(String tel) {
+        pTel = tel;
+    }
+    public void setMail(String mail) {
+        pMail = mail;
+    }
+    public void setGehalt(float gehAlt) {
+        gehalt = gehAlt;
+    }
+    
+    public int getPNr() {
+        return pNr;
+    }
+    public String getName() {
+        return pName;
+    }
+    public String getVName() {
+        return pVName;
+    }
+    /*public String getAdresse() {
+        ArrayList adresse;
+        adresse.add(pStrNr);
+        adresse.add(", ");        
+        adresse.add(pPLZ);
+        adresse.add(pOrt);
+        adresse.
+        return adresse;
+    }
+    */
+    public void getAdresse() {
+        System.out.println(pStrNr+", "+pPLZ+pOrt);
+    }
+    public String getStrNr() {
+        return pStrNr;
+    }
+    public int getPLZ() {
+        return pPLZ;
+    }
+    public String getOrt() {
+        return pOrt;
+    }
+    public Date getGebDat() {
+        return pGebDat;
+    }
+    public String getTel() {
+        return pTel;
+    }
+    public String getMail() {
+        return pMail;
+    }
+    public float getGehalt() {
+        return gehalt;
+    }
 }
