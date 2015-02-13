6e31e9d881b24a97eef3b3be4709602d811695c9
GFan99 <becksusanna@Susannas-MacBook-Pro.local>
2015-02-13 11:05:37 +0100

    main weiter testen

--- src/TierheimMain.java
+++ src/TierheimMain.java
@@ -11,10 +11,11 @@
 public class TierheimMain {
     public static void main(String[] args) {
 
-
-
     Pfleger pfleger1 = new Pfleger(1, "Maier", "Heinz", "Muehlweg 5", 12356, "Berlin", "12/04/1963", "0304573492", "HeinzMaier@googlemail.com", 1600.00f);
     Pfleger pfleger2 = new Pfleger(2, "Schmidt", "Anna", "Hauptstraße 12", 12384, "Berlin", "12.06.1985", "0304574462", "AnnaSchmidt@googlemail.com", 1450.00f);
+    System.out.println(pfleger2.getMail());
+    pfleger2.setMail("superanna313@web.de");
+    System.out.println(pfleger2.getMail());
     }
 
 }
